a4efd1933825e1e9ac6ac6c943ffa7c145d91052
abenezer <abenezerzewdu11@gmail.com>
2026-04-18 08:49:54 +0300

    created event

---
+++ app/Events/UserJoinedQueue.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\QueueEntry;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserJoinedQueue implements ShouldBroadcast
+{
+     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $entry;
+
+    public function __construct(QueueEntry $entry)
+    {
+        $this->entry = $entry;
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('queue.' . $this->entry->queue_id);
+    }
+
+    public function broadcastAs()
+    {
+        return 'user.joined';
+    }
+}
