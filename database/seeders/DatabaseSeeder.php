095bfe91f4548fe041de94f09d2f0f37e5c803d2
abenezer <abenezerzewdu11@gmail.com>
2026-04-23 23:49:45 +0300

    fixed roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
-        $this->call(QueueSeeder::class);
+        // $this->call(QueueSeeder::class);
+        $this->call(RoleSeeder::class);
     }
 }
